ca16156956f99a15b840fb447fe0576ce7a34e1e
Mincan Yang <49733113+ymcmatt@users.noreply.github.com>
2020-02-12 13:16:37 -0500

    Delete PS2 P2.js

--- PS2 P2.js
+++
@@ -1,9 +0,0 @@
-function* wordGen(sentence)
-{
-    yield* sentence.split(' ')
-}
-
-let a = wordGen('All I know is something like a bird within her sang');
-for (word of a){
-    console.log(word)
-}
