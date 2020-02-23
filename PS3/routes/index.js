47937166248b0b32d9ee10df170f9656e08683eb
Mincan Yang <ymcmatt@bu.edu>
2020-02-23 13:19:36 -0500

    PS3 1st commit

---
+++ PS3/routes/index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/ps3', function(req, res, next) {
+  res.render('index', { title: 'PS3' });
+});
+
+router.get('/example', function(req, res, next) {
+  res.render('example', { string: 'example' });
+});
+
+router.post('/3rdQuestion',function(req, res, next){
+  res.render('3rdQuestion',{
+    string: req.body['real'],
+    length: req.body['real'].length
+  })
+})
+
+module.exports = router;
