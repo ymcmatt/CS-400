c50a9f1e288918f9a38290ba73161e940348319f
Mincan Yang <ymcmatt@bu.edu>
2020-02-23 13:26:44 -0500

    2nd commit - fix mounting problem

--- PS3/routes/index.js
+++ PS3/routes/index.js
@@ -3,11 +3,8 @@ var router = express.Router();
 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
 
-router.get('/ps3', function(req, res, next) {
+router.get('/', function(req, res, next) {
   res.render('index', { title: 'PS3' });
 });
 
